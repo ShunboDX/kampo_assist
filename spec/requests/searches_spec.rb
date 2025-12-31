9256c24f781ea3a6e3d29df9f90e51133f059804
井上竣介 <shunbo@inoueshunsukenoMacBook-Pro.local>
2025-12-31 12:04:40 +0900

    Update request spec for login required step2

--- spec/requests/searches_spec.rb
+++ spec/requests/searches_spec.rb
@@ -9,9 +9,26 @@ RSpec.describe "Searches", type: :request do
   end
 
   describe "GET /step2" do
-    it "returns http success" do
-      get step2_search_path
-      expect(response).to have_http_status(:success)
+    context "when not logged in" do
+      it "redirects to login page" do
+        get step2_search_path
+        expect(response).to have_http_status(:found) # 302
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context "when logged in" do
+      let(:password) { "password" }
+      let(:user) { create(:user, password: password) }
+
+      before do
+        post user_session_path, params: { email: user.email, password: password }
+      end
+
+      it "returns http success" do
+        get step2_search_path
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
