2137a3ecde98797e375b27c0f8a38ed4e2ad300a
井上竣介 <shunbo@inoueshunsukenoMacBook-Pro.local>
2026-01-05 10:49:43 +0900

    Add favorite button to kampo detail page (#152)

--- app/controllers/kampos_controller.rb
+++ app/controllers/kampos_controller.rb
@@ -1,5 +1,6 @@
 class KamposController < ApplicationController
   def show
     @kampo = Kampo.find(params[:id])
+    @favorited = logged_in? && current_user.favorites.exists?(kampo_id: @kampo.id)
   end
 end
