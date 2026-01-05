656c77caf9bb69d116db886bdc76a569d1e0a96d
井上竣介 <shunbo@inoueshunsukenoMacBook-Pro.local>
2026-01-05 13:54:41 +0900

    Update favorite button via Turbo and prevent double submit (#153 #154)

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,38 @@ class FavoritesController < ApplicationController
   before_action :set_kampo
 
   def create
-    current_user.favorites.create!(kampo: @kampo)
-    redirect_back fallback_location: kampo_path(@kampo), notice: "お気に入りに追加しました"
+    begin
+      current_user.favorites.create!(kampo: @kampo)
+    rescue ActiveRecord::RecordNotUnique
+      # 連打や競合が起きても落とさない（すでに登録済みならOK扱い）
+    end
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "favorite_kampo_#{@kampo.id}",
+          partial: "kampos/favorite_button",
+          locals: { kampo: @kampo, favorited: true }
+        )
+      end
+      format.html { redirect_back fallback_location: kampo_path(@kampo), notice: "お気に入りに追加しました" }
+    end
   end
 
   def destroy
-    favorite = current_user.favorites.find_by!(kampo: @kampo)
-    favorite.destroy!
-    redirect_back fallback_location: kampo_path(@kampo), notice: "お気に入りを解除しました"
+    favorite = current_user.favorites.find_by(kampo: @kampo)
+    favorite&.destroy
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "favorite_kampo_#{@kampo.id}",
+          partial: "kampos/favorite_button",
+          locals: { kampo: @kampo, favorited: false }
+        )
+      end
+      format.html { redirect_back fallback_location: kampo_path(@kampo), notice: "お気に入りを解除しました" }
+    end
   end
 
   private
