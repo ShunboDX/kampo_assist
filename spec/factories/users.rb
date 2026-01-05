d83f215ed2f4268e51691fc2a4cd295ea6129221
井上竣介 <shunbo@inoueshunsukenoMacBook-Pro.local>
2026-01-05 16:47:30 +0900

    Add request specs for favorite CRUD (#156)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
   end
