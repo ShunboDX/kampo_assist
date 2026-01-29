f733f2c7cf5a34cbb80f82419df96dc94cedff11
井上竣介 <shunbo@inoueshunsukenoMacBook-Pro.local>
2026-01-29 14:14:36 +0900

    SearchUX(#225): 現在ステップの強調表示と戻る導線の文言を改善

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def search_step_current
+        case controller.action_name
+        when "step1"
+        1
+        when "step2"
+        2
+        when "results"
+        3
+        else
+        1
+        end
+    end
 end
