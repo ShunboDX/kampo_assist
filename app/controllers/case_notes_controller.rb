df59a929764e3ea3bb826d44c3c3afed1ac932da
井上竣介 <shunbo@inoueshunsukenoMacBook-Pro.local>
2026-02-18 04:00:53 +0900

    【Security】他ユーザー/管理者データにアクセスできないこと確認 #212

--- app/controllers/case_notes_controller.rb
+++ app/controllers/case_notes_controller.rb
@@ -16,11 +16,12 @@ class CaseNotesController < ApplicationController
   def create
     @case_note = current_user.case_notes.build(case_note_params)
 
-    if @case_note.save
-      redirect_to case_notes_path, notice: "症例メモを作成しました"
-    else
-      render :new, status: :unprocessable_entity
-    end
+  if @case_note.search_session_id.present? &&
+     !current_user.search_sessions.exists?(id: @case_note.search_session_id)
+    @case_note.errors.add(:search_session_id, "が不正です")
+    return render :new, status: :unprocessable_entity
+  end
+
   end
 
   def edit
