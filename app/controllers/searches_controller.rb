d80285f58c32391f56d89fefef7ef1975408e7e0
井上竣介 <shunbo@inoueshunsukenoMacBook-Pro.local>
2026-02-18 02:16:26 +0900

    Refactor: move SearchSession saving to SearchSessionSaver service

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -60,7 +60,12 @@ class SearchesController < ApplicationController
     end
 
     @show_login_prompt = !logged_in?
-    @search_session = save_search_session!(medical_area_ids:, disease_ids:, symptom_ids:)
+    @search_session = SearchSessionSaver.new(
+      user: current_user,
+      medical_area_ids: medical_area_ids,
+      disease_ids: disease_ids,
+      symptom_ids: symptom_ids
+    ).call
 
     if logged_in? && @search_session.present?
       @search_case_note = current_user.case_notes.find_or_initialize_by(
@@ -86,36 +91,6 @@ class SearchesController < ApplicationController
 
   def symptom_ids_param
     array_param(:symptom_ids)
-  end
-
-  def save_search_session!(medical_area_ids:, disease_ids:, symptom_ids:)
-    return unless current_user
-
-    conditions = {
-      "medical_area_ids" => medical_area_ids,
-      "disease_ids"      => disease_ids,
-      "symptom_ids"      => symptom_ids
-    }.compact_blank
-
-    return if conditions.blank?
-
-    hash = SearchSession.generate_hash(conditions)
-
-    search_session = current_user.search_sessions.find_or_initialize_by(conditions_hash: hash)
-    search_session.conditions = conditions
-    search_session.save!
-
-    enforce_search_session_limit!(current_user, 100)
-
-    search_session
-  end
-
-  def enforce_search_session_limit!(user, limit)
-    over = user.search_sessions.count - limit
-    return if over <= 0
-
-    # 集約運用なので updated_at が古い順に消すと自然
-    user.search_sessions.order(updated_at: :asc).limit(over).delete_all
   end
 
   def require_login
