88d12b6d6ebbcd39ec3b38f860255e974a0fac21
井上竣介 <shunbo@inoueshunsukenoMacBook-Pro.local>
2026-02-18 12:50:49 +0900

    add ja

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module KampoAssist
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # i18n（日本語をデフォルトにする）
+    config.i18n.default_locale = :ja
   end
 end
