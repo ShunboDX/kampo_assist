cb3d80078c17a85ea7a41732dba80a2a130eb09f
井上竣介 <shunbo@inoueshunsukenoMacBook-Pro.local>
2026-03-15 15:24:08 +0900

    fix: stabilize favorite creation and listing

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,14 @@
 class FavoritesController < ApplicationController
   before_action :require_login
-  before_action :set_kampo, only: [ :create, :destroy ]
+  before_action :set_kampo, only: %i[create destroy]
 
   def index
-    @kampos = current_user.favorite_kampos.order(:id)
+    @favorites = current_user.favorites.includes(:kampo).order(created_at: :desc)
+    @kampos = @favorites.map(&:kampo)
   end
 
   def create
-    begin
-      current_user.favorites.create!(kampo: @kampo)
-    rescue ActiveRecord::RecordNotUnique
-      # 連打や競合が起きても落とさない（すでに登録済みならOK扱い）
-    end
+    current_user.favorites.find_or_create_by!(kampo: @kampo)
 
     respond_to do |format|
       format.turbo_stream do
@@ -21,13 +18,14 @@ class FavoritesController < ApplicationController
           locals: { kampo: @kampo, favorited: true }
         )
       end
-      format.html { redirect_back fallback_location: kampo_path(@kampo), notice: "お気に入りに追加しました" }
+      format.html do
+        redirect_back fallback_location: kampo_path(@kampo), notice: "お気に入りに追加しました"
+      end
     end
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(kampo: @kampo)
-    favorite&.destroy
+    current_user.favorites.find_by(kampo: @kampo)&.destroy
 
     respond_to do |format|
       format.turbo_stream do
@@ -37,7 +35,9 @@ class FavoritesController < ApplicationController
           locals: { kampo: @kampo, favorited: false }
         )
       end
-      format.html { redirect_back fallback_location: kampo_path(@kampo), notice: "お気に入りを解除しました" }
+      format.html do
+        redirect_back fallback_location: kampo_path(@kampo), notice: "お気に入りを解除しました"
+      end
     end
   end
 
